9336b6ffa228e389a2ca075b9827fe006afab366
Peter Ahn <xion486@gmail.com>
2019-01-09 11:45:08 +0900

    ScreenAPI upload

---
+++ ScreenAPI.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <Windows.h>
+
+static int g_nScreenIndex;
+static HANDLE g_hScreen[2];
+
+void ScreenInit()
+{
+	g_hScreen[0] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	g_hScreen[1] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+}
+
+void ScreenFlipping()
+{
+	SetConsoleActiveScreenBuffer(g_hScreen[g_nScreenIndex]);
+	g_nScreenIndex = !g_nScreenIndex;
+}
+
+void ScreenClear()
+{
+	COORD Coor = { 0, 0 };
+	DWORD dw;
+	FillConsoleOutputCharacter(g_hScreen[g_nScreenIndex], ' ', 0x1000, Coor, &dw);
+}
+
+void ScreenRelease()
+{
+	CloseHandle(g_hScreen[0]);
+	CloseHandle(g_hScreen[1]);
+}
+
+void ScreenPrint(int x, int y, const char *text) 
+{
+	DWORD dw;
+	COORD CursorPosition = { (SHORT)x, (SHORT)y };
+	WriteConsoleOutputCharacter(g_hScreen[g_nScreenIndex], text, strlen(text), CursorPosition, &dw);
+}
+
+void SetColor(unsigned short color)
+{
+	SetConsoleTextAttribute(g_hScreen[g_nScreenIndex], color);
+}
